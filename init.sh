4389cd7e380237ff8127607404b2d2372a7b0eab
Mikhail <alexeevmihail16@gmail.com>
2020-02-26 21:38:36 +0300

    update

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+mkdir public
+mkdir uploads
+cd public
+mkdir img
+mkdir css
+mkdir js
+sudo rm -rf /etc/ngix/sites-enabled
+sudo cp -f /home/box/web/etc/nginx.conf /etc/nginx 
 sudo /etc/init.d/nginx restart
