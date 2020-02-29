f387170c1fc86589abaf6111f1988dd749d650cb
Mikhail <alexeevmihail16@gmail.com>
2020-03-01 00:07:27 +0300

    update

--- init.sh
+++ init.sh
@@ -6,7 +6,6 @@ mkdir css
 mkdir js
 sudo rm -rf /etc/ngix/sites-enabled
 sudo cp -f /home/box/web/etc/nginx.conf /etc/nginx
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart 
+sudo cp -sf /home/box/web/hello.py /etc/gunicorn.d
 sudo /etc/init.d/nginx restart
 sudo gunicorn -b 0.0.0.0:8080 hello.py
