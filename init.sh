e39578d4ae7a00987da0b60e81997ad970399116
Mikhail <alexeevmihail16@gmail.com>
2020-03-01 00:25:45 +0300

    update

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ mkdir js
 sudo rm -rf /etc/ngix/sites-enabled
 sudo cp -f /home/box/web/etc/nginx.conf /etc/nginx
 sudo cp -sf /home/box/web/hello.py /etc/gunicorn.d
-gunicorn -b 0.0.0.0:8080 hello 
+gunicorn -b 0.0.0.0:8080 hello
+gunicorn -c hello.py hello:app 
 sudo /etc/init.d/nginx restart
 
