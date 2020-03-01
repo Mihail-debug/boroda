05583b3ced429499266630466975dfe4564258f2
Mikhail <alexeevmihail16@gmail.com>
2020-03-01 13:39:35 +0300

    update

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
     status = '200 OK'
     data = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    headers = [
+    header = [
         ('Content-Type', 'text/plain')
     ]
     start_response(status, header)
--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@ sudo rm -rf /etc/ngix/sites-enabled
 sudo cp -f /home/box/web/etc/nginx.conf /etc/nginx
 sudo cp -sf /home/box/web/hello.py /etc/gunicorn.d
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
+gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
