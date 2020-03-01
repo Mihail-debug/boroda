dfd136dad5d5d789dfb776fe7e943c2844c9dc36
Mikhail <alexeevmihail16@gmail.com>
2020-03-01 13:51:39 +0300

    update

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ mkdir img
 mkdir css
 mkdir js
 sudo rm -rf /etc/ngix/sites-enabled
+sudo rm -rf /etc/nginx/sites-available/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo cp -f /home/box/web/etc/nginx.conf /etc/nginx
 sudo cp -sf /home/box/web/hello.py /etc/gunicorn.d
 sudo /etc/init.d/nginx restart
