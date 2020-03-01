6ca88f9459c1bb1b9f989fad9269ec4f9f7c0688
Mikhail <alexeevmihail16@gmail.com>
2020-03-01 14:02:18 +0300

    up

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def app(environ, start_response):
     status = '200 OK'
     data = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     header = [
-        ('Content-Type', 'text/plain')
+        ('Content-Type', "text/plain; charset=utf-8")
     ]
     start_response(status, header)
     return data
